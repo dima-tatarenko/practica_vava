4d1593ae4b740d58af35c740e41a20c8a14db63e
Dima_Tatarenko <dmitriy.tatarenko@gmail.com>
2023-10-07 22:29:34 +0200

    Added a drop-down JS menu to mobile. aka -> El condicional termario ataca de nuevo.

--- js/nav_load.js
+++ js/nav_load.js
@@ -5,3 +5,8 @@ $(function () {
 $(function () {
     $("#footer-placeholder").load("footer.html");
 });
+
+function dropDown() {
+    let navMenu = document.getElementById("navLinks");
+    (navMenu.style.display === "block") ? navMenu.style.display = "none" : navMenu.style.display = "block"
+};
