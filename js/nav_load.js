b2905594e80cb6b2e956287f89594d459cd7e817
Dima_Tatarenko <dmitriy.tatarenko@gmail.com>
2023-10-08 16:43:29 +0200

    Added an on-click function for -the way we work- Now each button displays a different text and makes everyone happy.

--- js/nav_load.js
+++ js/nav_load.js
@@ -10,3 +10,28 @@ function dropDown() {
     let navMenu = document.getElementById("navLinks");
     (navMenu.style.display === "block") ? navMenu.style.display = "none" : navMenu.style.display = "block"
 };
+
+function showElement(pItem, pParagraph) {
+    let seeElement = document.getElementById(pParagraph)
+    let changeItem = document.getElementById(pItem)
+
+    liDiscover.style.border = "0rem";
+    liAnalyse.style.border = "0rem";
+    liImprove.style.border = "0rem";
+    liMeasure.style.border = "0rem";
+    liControl.style.border = "0rem";
+    changeItem.style.border = "0rem"
+    if (changeItem.style.border == "0rem") {
+        changeItem.style.borderBottom = "solid";
+    }
+
+    discover.style.display = "none";
+    analyse.style.display = "none";
+    improve.style.display = "none";
+    measure.style.display = "none";
+    control.style.display = "none";
+    seeElement.style.display = "none";
+    if (seeElement.style.display === "none") {
+        seeElement.style.display = "block";
+    }
+};
